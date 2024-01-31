da65c411ecb0c610e1293908107c6e267eddcd92
bBraian <braianvoficial@gmail.com>
2024-01-30 21:26:48 -0300

    feat: services and error treatments

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -2,12 +2,13 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from "zod"
 import { RegisterService } from '../services/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '../services/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
     name: z.string(),
     email: z.string().email(),
-    password: z.string()
+    password: z.string().min(6)
   })
 
   const { name, email, password } = registerBodySchema.parse(request.body)
@@ -18,7 +19,11 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
     await registerService.execute({ name, email, password })
   } catch (error) {
-    return reply.status(409).send()
+    if(error instanceof UserAlreadyExistsError) {
+      return reply.status(409).send({ message: error.message })
+    } 
+
+    throw error
   }
 
   return reply.status(201).send()

c320b040b715f1a79620eea10eb22468bf7152ae
bBraian <braianvoficial@gmail.com>
2024-02-19 21:36:27 -0300

    feat: fetch checkins user history service

---
+++ src/http/services/fetch-user-check-ins-history.ts
@@ -0,0 +1,21 @@
+import { CheckIn } from "@prisma/client";
+import { CheckInsRepository } from "@/repositories/checkins-repository";
+
+interface FetchUserCheckInHistoryServiceRequest {
+  userId: string;
+  page: number;
+}
+
+interface FetchUserCheckInHistoryServiceResponse {
+  checkIns: CheckIn[]
+}
+
+export class FetchUserCheckInHistoryService {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({ userId, page }: FetchUserCheckInHistoryServiceRequest): Promise<FetchUserCheckInHistoryServiceResponse> {
+    const checkIns = await this.checkInsRepository.findManyByUserId(userId, page)
+    
+    return { checkIns }
+  }
+}

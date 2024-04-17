ea8b7e685ed4aa8b723557bd3f6e91e4b22d76a3
bBraian <braianvoficial@gmail.com>
2024-04-16 21:47:45 -0300

    feat: controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,16 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserProfileService } from '../services/factories/make-get-user-profile-service'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
+  const getUserProfile = makeGetUserProfileService()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub
+  })
 
-  console.log(request.user.sub)
-
-  return reply.status(200).send()
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined
+    }
+  })
 }

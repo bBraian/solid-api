f793a186dde8e089e85b397c877227131bdbfafc
bBraian <braianvoficial@gmail.com>
2024-04-22 21:06:50 -0300

    feat: separating routes and creatting gyms controller

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -1,9 +1,9 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from "zod"
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
-import { AuthenticateService } from '../services/authenticate'
-import { InvalidCredentialsError } from '../services/errors/invalid-credentials-error'
-import { makeAuthenticateService } from '../services/factories/make-authenticate-service'
+import { AuthenticateService } from '../../services/authenticate'
+import { InvalidCredentialsError } from '../../services/errors/invalid-credentials-error'
+import { makeAuthenticateService } from '../../services/factories/make-authenticate-service'
 
 export async function authenticate(request: FastifyRequest, reply: FastifyReply) {
   const authenticateBodySchema = z.object({
